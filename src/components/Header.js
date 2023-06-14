2fa7f3b3ebf2c3a71f906d0288162c5401d9f041
yhyem <yoonhyemin0120@gmail.com>
2023-06-14 10:27:15 +0900

    feat: 헤더 구현 setting: 개인정보 수집 동의서 데이터 세팅

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,23 @@
+import { styled } from "styled-components";
+
+import logo from "../assets/images/logo.svg";
+
 const Header = () => {
-  return <></>;
+  return (
+    <HeaderBlock>
+      <Logo src={logo}></Logo>
+    </HeaderBlock>
+  );
 };
 
 export default Header;
+
+const HeaderBlock = styled.div`
+  width: 100%;
+  height: 116px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.GRAY};
+`;
+
+const Logo = styled.img`
+  margin: 41px 0 41px 363px;
+`;
